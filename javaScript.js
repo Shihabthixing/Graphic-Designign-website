e91a522ce5d300384792ac871f273f6fdb17fbec
Shihabxing <xingforgraphics@gmail.com>
2020-09-29 23:23:31 +0300

    my photo aded

--- javaScript.js
+++ javaScript.js
@@ -4,6 +4,7 @@
 
         menuBtn.onclick = function(){
 
-            document.getElementById("menu").classList.toggle("change")
-     }
+            document.getElementById("menu").classList.toggle("change");
+            document.getElementById("mobMenu").classList.toggle("change");
+        }
 
